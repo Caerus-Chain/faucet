70cbcfe1a7c2f508b790ff37bef62b12710ef678
solidity12 <volibear1001@gmail.com>
2023-09-23 21:48:39 +0900

    feat: Add faucet address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -19,3 +19,5 @@ main()
     console.error(error);
     process.exit(1);
   });
+// Deploying contracts with the account: 0xa40aa030A3ba4f42FDCd2B7bC33d5B03770290ea
+// Faucet contract deployed to: 0xF647E71bb4704De8E413166ebcA875c4ea0f2480
