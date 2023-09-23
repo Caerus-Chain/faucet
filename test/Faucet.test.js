f2f62869d9df33583040651b9baf4775abd7030f
solidity12 <volibear1001@gmail.com>
2023-09-23 21:31:45 +0900

    feat: Add test script

---
+++ test/Faucet.test.js
@@ -0,0 +1,60 @@
+const { ethers, waffle } = require("hardhat");
+const { expect } = require("chai");
+
+describe("Faucet contract", function () {
+  let Faucet, faucet, owner, addr1, addr2;
+
+  beforeEach(async function () {
+    Faucet = await ethers.getContractFactory("Faucet");
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    // Deploy the Faucet contract
+    faucet = await Faucet.deploy();
+    await faucet.deployed();
+  });
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await faucet.owner()).to.equal(owner.address);
+    });
+
+    it("Should have zero balance initially", async function () {
+      expect(await faucet.getBalance()).to.equal(0);
+    });
+  });
+
+  describe("Funding and Withdrawing", function () {
+    it("Should accept ether from anyone", async function () {
+      await addr1.sendTransaction({
+        to: faucet.address,
+        value: ethers.utils.parseEther("1.0"),
+      });
+      expect(await faucet.getBalance()).to.equal(ethers.utils.parseEther("1.0"));
+    });
+
+    it("Should allow the owner to withdraw", async function () {
+      await addr1.sendTransaction({
+        to: faucet.address,
+        value: ethers.utils.parseEther("1.0"),
+      });
+      await faucet.connect(owner).withdraw(ethers.utils.parseEther("0.5"));
+      expect(await faucet.getBalance()).to.equal(ethers.utils.parseEther("0.5"));
+    });
+  });
+
+  describe("Get Test Ether", function () {
+    it("Should send test ether", async function () {
+      await addr1.sendTransaction({
+        to: faucet.address,
+        value: ethers.utils.parseEther("1.0"),
+      });
+  
+      const initialBalance = await ethers.provider.getBalance(addr2.address);
+      await faucet.connect(addr2).getTestEther();
+      const finalBalance = await ethers.provider.getBalance(addr2.address);
+  
+      const difference = finalBalance.sub(initialBalance);
+      expect(difference).to.be.closeTo(ethers.utils.parseEther("0.5"), ethers.utils.parseEther("0.01"));
+    });
+  });
+});
